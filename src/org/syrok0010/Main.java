b4acb0a016cda9ceb5aacdbe5107495cdab1975d
syrok0010 <steamruler@mail.ru>
2024-04-09 19:33:20 +0300

    Add date of birth error message

--- src/org/syrok0010/Main.java
+++ src/org/syrok0010/Main.java
@@ -1,15 +1,56 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package org.syrok0010;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        var scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String lastName = getValidName("фамилию", scanner);
+        String name = getValidName("имя", scanner);
+        String middleName = getValidName("отчество", scanner);
+
+        LocalDate dateOfBirth = getValidDateOfBirth(scanner);
+
+        System.out.print(
+                lastName + ' ' + name.charAt(0) + '.' + middleName.charAt(0) + '.' + '\n' +
+                "Пол: " + (middleName.endsWith("на") ? "женский" : "мужской") + '\n' +
+                "Возраст: " + getValidAgeString(Period.between(dateOfBirth, LocalDate.now()).getYears())
+        );
+    }
+
+    private static String getValidAgeString(int age) {
+        if (age >= 10 && age <= 20) return age + " лет";
+        return switch (age % 10) {
+            case 1 -> age + " год";
+            case 2, 3, 4 -> age + " года";
+            default -> age + " лет";
+        };
+    }
+
+    private static LocalDate getValidDateOfBirth(Scanner scanner) {
+        LocalDate res = null;
+        do {
+            try {
+                System.out.print("Введите дату рождения (дд.мм.гггг):");
+                res = LocalDate.parse(scanner.next(), DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+            }
+            catch (Exception ignored){
+                System.out.println("Дата рождения введена неверно, повторите попытку.");
+            }
+        } while (res == null || res.isAfter(LocalDate.now()));
+        return res;
+    }
+
+    private static String getValidName(String nameType, Scanner scanner) {
+        String res;
+        do {
+            System.out.printf("Введите %s: ", nameType);
+            res = scanner.next().trim();
+        } while (res.isBlank() || !res.chars().allMatch(Character::isLetter));
+        return res.substring(0, 1).toUpperCase() + res.substring(1).toLowerCase();
     }
 }
